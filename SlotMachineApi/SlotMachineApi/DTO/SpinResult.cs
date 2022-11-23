a4ab25c020de5e22e65cc4ea642ef5071709738e
mershalova <angelamersh@gmail.com>
2022-11-23 12:44:05 +0200

    game Service

--- SlotMachineApi/SlotMachineApi/DTO/SpinResult.cs
+++ SlotMachineApi/SlotMachineApi/DTO/SpinResult.cs
@@ -5,6 +5,7 @@ namespace SlotMachineApi.DTO
     public class SpinResult
     {
         public int[] Slots { get; set; } 
-        Player Player { get; set; }
+        public double Balance { get; set; }
+        public double Win { get; set; }
     }
 }
---
+++ SlotMachineApi/SlotMachineApi/Services/Impl/GameService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SlotMachineApi.DbSettings;
+using SlotMachineApi.Entities;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SlotMachineApi.Services.Impl
+{
+    public class GameService: IGameService
+    {
+        private readonly IMongoCollection<Game> _gameCollection;
+
+        public GameService(
+          IOptions<SlotMachineDatabaseSettings> slotMachineDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                slotMachineDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                slotMachineDatabaseSettings.Value.DatabaseName);
+
+            _gameCollection = mongoDatabase.GetCollection<Game>(
+            slotMachineDatabaseSettings.Value.GamesCollectionName);
+        }
+        public int[] ReturnSlotsArray(Game game)
+        {
+            Random randNum = new Random();
+
+            int[] slotsArray = Enumerable
+                .Repeat(0, game.SlotsSize)
+                .Select(i => randNum.Next(0, 9))
+                .ToArray();
+            return slotsArray;
+        }
+
+        public async Task Update(Game game, int newSize)
+        {
+            game.SlotsSize = newSize;
+
+            await _gameCollection.ReplaceOneAsync(x=> x.Id.Equals(game.Id), game);
+        }
+    }
+}
