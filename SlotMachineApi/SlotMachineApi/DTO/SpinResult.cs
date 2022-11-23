71f176e62915a926a003cbbbad2c60635f90f9f5
mershalova <angelamersh@gmail.com>
2022-11-23 12:18:23 +0200

    SpinResult

--- SlotMachineApi/SlotMachineApi/DTO/SpinResult.cs
+++ SlotMachineApi/SlotMachineApi/DTO/SpinResult.cs
@@ -1,8 +1,10 @@
-﻿namespace SlotMachineApi.DTO
+﻿using SlotMachineApi.Entities;
+
+namespace SlotMachineApi.DTO
 {
     public class SpinResult
     {
         public int[] Slots { get; set; } 
-
+        Player Player { get; set; }
     }
 }
